d520b604bc20c222ed2ad7875616401abf48ba14
Josue Iglesias <jiglesias3@ucol.mx>
2022-05-22 02:46:12 -0500

    Tabla de usuarios y botón delete

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, doc, setDoc, where, query, getDocs, getDocFromCache, getDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAjzD-oDcLBCCuBP2vAil4o9UAEx0ght5k",
@@ -17,4 +17,36 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth();
-const firestore = getFirestore();
+export const db = getFirestore();
+export {doc, collection, getDocFromCache, getDoc}
+export const deleteContacts = id => deleteDoc(doc(db, 'users', id))
+
+/*
+export const newUser = (correo, rol, area) => {
+  addDoc(collection(db, 'usuarios'), {
+    correo,
+    rol,
+    area
+  })
+}
+
+*/
+
+
+/*
+export const newUser = (id, correo, rol, area) => {
+
+  db.collection('usuarios').doc(id).set({
+    correo,
+    rol,
+    area
+  })
+
+}
+*/
+
+export const newUser = async (id, email, role, name, phone, area, active) => {
+
+  await setDoc(doc(db, 'users', id), {email, role, name, phone, area, active})
+
+}
--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
 import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+import { getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js"
+import {newUser, db, doc, collection, getDocFromCache, getDoc, deleteContacts} from './firebase.js'
 //console.log(auth);
 const signupForm = document.querySelector('#signup-form')
 const auth = getAuth();
+const contactsContainer = document.getElementById('table')
 
 //sign up
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const signupEmail = document.querySelector('#signup-email').value
     const signupPassword = document.querySelector('#signup-password').value
+    const signupName = document.querySelector('#signup-name').value
+    const signupPhone = document.querySelector('#signup-phone').value
+    const signupArea = document.querySelector('#signup-area').value
+
 
         createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
         .then(userCredential => {
@@ -18,7 +25,9 @@ signupForm.addEventListener('submit', (e) => {
             //hide modal
             $('#signUpModal').modal('hide')
             //signUpModal.hide()
-            console.log('sign up AAA')    
+            console.log('sign up AAA')
+            console.log(userCredential.user.uid);
+            newUser(userCredential.user.uid, signupEmail, 'user', signupName, signupPhone, signupArea, false)    
         }).catch (error => {
             console.log(error.code)
             switch(error.code) {
@@ -36,7 +45,9 @@ signupForm.addEventListener('submit', (e) => {
                     break;              
             }
         })
-})
+
+        
+    })
 
 //login
 const loginForm = document.querySelector('#login-form')
@@ -93,12 +104,72 @@ const signUpButton = document.querySelector('#signUpButton')
 const logOutButton = document.querySelector('#logout')
 const message = document.querySelector('#welcome-message')
 
-auth.onAuthStateChanged(user => {
+
+auth.onAuthStateChanged(async user => {
     if(user){
         loginButton.hidden = true
         signUpButton.hidden = true
         logOutButton.hidden = false
-        message.hidden = false
+
+        //const q = query(usersRef, where(, "==", "CA"));
+        const usersRef = collection(db, "users")
+        // console.log(user.uid);
+        // console.log(doc(db, "users", user.uid));
+        // console.log(doc(usersRef, user.uid));
+        
+        
+            
+            // const docRef = doc(db, "users", user.uid);
+            // const docSnap = await getDoc(docRef);
+            // const userName = docSnap.data().name
+            // const userRole = docSnap.data().Role
+            
+            // console.log("Document data: ");
+            // console.log(docSnap.data());
+            // console.log("User name: " + userName);
+
+        
+        
+            const query = await getDocs(collection(db, "users"));
+            query.forEach( (doc) => {
+                 console.log(doc.id, " => ", doc.data());
+                 // message.innerHTML = (`User: ${doc.id} => ${doc.data().role}`)
+                 document.getElementById('table').innerHTML += 
+               
+                 `<tr>
+                    <td style="border-width: 1px"> ${doc.data().name}</td> 
+                    <td style="border-width: 1px"> ${doc.data().email}</td> 
+                    <td style="border-width: 1px"> ${doc.data().phone}</td> 
+                    <td style="border-width: 1px"> ${doc.data().role}</td> 
+                    <td style="border-width: 1px"> ${doc.data().area}</td>
+                    <td style="border-width: 1px">
+                    <button class='editBtn' data-id="${doc.id}">Edit</button>
+                    <button class='delBtn' data-id="${doc.id}">Delete</button></td>
+                 </tr>`
+            });
+
+        const btnsDelete = contactsContainer.querySelectorAll('.delBtn')
+        const btnsEdit = contactsContainer.querySelectorAll('.editBtn')
+        console.log(btnsDelete)
+
+        btnsDelete.forEach(btn =>{
+            btn.addEventListener('click', ({target: {dataset}}) => {
+                // console.log(dataset.id)
+                deleteContacts(dataset.id)
+                window.location.reload();
+            })
+        })
+
+        btnsEdit.forEach(btn =>{
+            btn.addEventListener('click', ({target: {dataset}}) => {
+                console.log(dataset.id)
+            })
+        })
+        
+        
+        // console.log(usersRef);
+
+
         message.innerHTML = `Bienvenido: ${user.email}`
     } else {
         loginButton.hidden = false
@@ -107,3 +178,4 @@ auth.onAuthStateChanged(user => {
         message.hidden = true
     }
 })
+
